8f323bb59f96990e92e21a2775d87ac9d1ab1e44
Jingjing_Q <52308983+JingQ7@users.noreply.github.com>
2019-09-17 02:04:07 -0400

    Update pipelines.py

--- scrap_rew/pipelines.py
+++ scrap_rew/pipelines.py
@@ -7,14 +7,16 @@ class RewhousePipeline(object):
 
         db = pymysql.connect(host='localhost', user='root', password='root', database='rew')
         cursor = db.cursor()
-        #cursor.execute('create table house_info (price varchar(100), addr varchar(200), link varchar(200))')
+        #cursor.execute('create table house_info (price varchar(100), sqft varchar(100), addr varchar(200), link varchar(200))')
 
         for i in range(0, len(item['addr'])):
             price = item['price'][i]
+            sqft = item['sqft'][i]
             addr = item['addr'][i]
             link = item['link'][i]
+
             try:
-                sql = 'insert into house_info(price, addr, link) values("'+price+'", "'+addr+'", "'+link+'")'
+                sql = 'insert into house_info(price, sqft, addr, link) values("'+price+'", "'+sqft+'", "'+addr+'", "'+link+'")'
                 cursor.execute(sql)
                 db.commit()
             except Exception as e:
